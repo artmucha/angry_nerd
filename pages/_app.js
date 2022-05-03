e115787bdb122f08d0f4039cd0859a4f4148204a
Artur Mucha <arti.mucha@gmail.com>
2022-05-03 11:56:24 +0200

    add dependencies and config for material ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,28 @@
-const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
-}
 
-export default App;
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import { CacheProvider } from '@emotion/react';
+import theme from 'styles/theme';
+import createEmotionCache from 'styles/createEmotionCache';
+import GlobalStyles from 'styles/globalStyles';
+
+const clientSideEmotionCache = createEmotionCache();
+
+const ClientApp = props => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>Angry Nerd</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+};
+
+export default ClientApp;
