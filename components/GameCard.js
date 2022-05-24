b88aa7201c25ffef3db88daf6332f7154b149a13
Artur Mucha <arti.mucha@gmail.com>
2022-05-24 07:49:16 +0200

    update gamecard component

--- components/GameCard.js
+++ components/GameCard.js
@@ -3,41 +3,47 @@ import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import Chip from '@mui/material/Chip';
 
 import Link from 'next/link';
 import Image from 'next/image';
 
 const GameCard = ({ game }) => {
   return (
-    <Grid item xs={12} sm={6} md={3}>
-      <Link href={'/'}>
+    <Grid item xs={12} sm={6} md={4}>
+      <Link href={`/gry/playstation4/${game.attributes.slug}`}>
         <a title={game.attributes.title}>
           <Card sx={{ display: 'flex', p: 2 }}>
             <Box sx={{ 
-              position: 'relative', 
               display: 'flex', 
               flexDirection: 'column', 
-              minWidth: 120, 
-              width: 120, 
-              height: 120 
+              alignItems: 'center'
             }}>
-              <Image
-                src={game.attributes.cover}
-                alt={game.attributes.title}
-                layout="fill"
-                objectFit="cover"
-              />
+              <Box sx={{ 
+                position: 'relative', 
+                minWidth: 100, 
+                width: 100, 
+                height: 100 
+              }}>
+                <Image
+                  src={game.attributes.cover}
+                  alt={game.attributes.title}
+                  layout="fill"
+                  objectFit="cover"
+                />
+              </Box>
+              { game.attributes.rating && 
+                <Chip label={game.attributes.rating} color="primary" variant="outlined" sx={{ mt: 1, px: 1 }} />
+              }
             </Box>
-            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-              <CardContent sx={{ flex: '1 0 auto', pt: 0, pr: 0 }}>
-                <Typography component="h3" variant="subtitle1">
-                  {game.attributes.title}
-                </Typography>
-                <Typography component="h4" variant="subtitle2" color="text.disabled">
-                  Wydano: {game.attributes.released}
-                </Typography>
-              </CardContent>
-            </Box>
+            <CardContent sx={{ flex: '1 0 auto', pt: 0, pr: 0, pl: 2 }}>
+              <Typography component="h3" variant="subtitle1">
+                {game.attributes.title}
+              </Typography>
+              <Typography component="h4" variant="subtitle2" color="text.disabled">
+                Wydano: {game.attributes.released}
+              </Typography>
+            </CardContent>
           </Card>
         </a>
       </Link>
