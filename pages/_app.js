bfc1ed0c6e08c72fab8924c8ecb94c6c0a72a9df
Artur Mucha <arti.mucha@gmail.com>
2022-06-19 13:16:22 +0200

    clean _app and _document files

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,17 @@
 
 import Head from 'next/head';
-import { ThemeProvider } from '@mui/material/styles';
-import { CacheProvider } from '@emotion/react';
-import theme from 'styles/theme';
-import createEmotionCache from 'styles/createEmotionCache';
-import GlobalStyles from 'styles/globalStyles';
-
-const clientSideEmotionCache = createEmotionCache();
 
 const ClientApp = props => {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  const { Component, pageProps } = props;
 
   return (
-    <CacheProvider value={emotionCache}>
+    <>
       <Head>
         <title>Angry Nerds - oceniamy gry</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </CacheProvider>
+      <Component {...pageProps} />
+      </>
   );
 };
 
--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,6 @@
 
 import * as React from 'react';
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
-import createEmotionServer from '@emotion/server/create-instance';
-import theme from 'styles/theme';
-import createEmotionCache from 'styles/createEmotionCache';
 
 export default class Document extends NextDocument {
   render() {
@@ -27,29 +24,18 @@ export default class Document extends NextDocument {
 
 Document.getInitialProps = async (ctx) => {
   const originalRenderPage = ctx.renderPage;
-  const cache = createEmotionCache();
-  const { extractCriticalToChunks } = createEmotionServer(cache);
 
   ctx.renderPage = () =>
     originalRenderPage({
       enhanceApp: (App) =>
         function EnhanceApp(props) {
-          return <App emotionCache={cache} {...props} />;
+          return <App {...props} />;
         },
     });
 
   const initialProps = await NextDocument.getInitialProps(ctx);
-  const emotionStyles = extractCriticalToChunks(initialProps.html);
-  const emotionStyleTags = emotionStyles.styles.map((style) => (
-    <style
-      data-emotion={`${style.key} ${style.ids.join(' ')}`}
-      key={style.key}
-      dangerouslySetInnerHTML={{ __html: style.css }}
-    />
-  ));
 
   return {
     ...initialProps,
-    styles: [...React.Children.toArray(initialProps.styles), ...emotionStyleTags],
   };
 };
