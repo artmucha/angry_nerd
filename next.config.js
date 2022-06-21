59e7cdc48fe72269440f18360c540fb4d7835225
Artur Mucha <arti.mucha@gmail.com>
2022-06-21 20:10:11 +0200

    update config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPWA = require('next-pwa')
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ['localhost', 'media.rawg.io'],
   },
-}
-
-module.exports = nextConfig;
+});
