c16e33cbf481ea189a7999b739b2866f247f461b
Artur Mucha <arti.mucha@gmail.com>
2022-06-23 17:29:31 +0200

    add seo config

---
+++ next-seo.config.js
@@ -0,0 +1,23 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'pl_PL',
+    url: 'https://angrynerds.pl',
+    site_name: 'Angry Nerds',
+    title: 'Angry Nerds',
+    images: [
+      {
+        url:
+          '/public/logo.png',
+        width: 2560,
+        height: 1440,
+        alt: 'Angry Nerds'
+      }
+    ]
+  },
+  twitter: {
+    handle: '@ArtiMucha',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+}
