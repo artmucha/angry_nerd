76d1b7465582072d24e4d49b7d3a744cecb741e2
Artur Mucha <arti.mucha@gmail.com>
2022-07-05 17:06:58 +0200

    update config file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require('next-pwa')
-const isProd = process.env.NODE_ENV === 'production'
+const withPWA = require('next-pwa');
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = withPWA({
   pwa: {
