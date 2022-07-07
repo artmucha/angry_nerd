32585eb16d7455dd0f9741eb99cf561cc9f51f25
Artur Mucha <arti.mucha@gmail.com>
2022-07-07 12:43:56 +0200

    add button component

---
+++ components/Button/index.js
@@ -0,0 +1,28 @@
+import { forwardRef } from 'react';
+import * as S from './styles';
+
+const Button = (
+  {
+    children,
+    icon,
+    size = 'medium',
+    fullWidth = false,
+    minimal = false,
+    ...props
+  },
+  ref
+) => (
+  <S.Wrapper
+    size={size}
+    fullWidth={fullWidth}
+    hasIcon={!!icon}
+    minimal={minimal}
+    ref={ref}
+    {...props}
+  >
+    {icon}
+    {!!children && <span>{children}</span>}
+  </S.Wrapper>
+)
+
+export default forwardRef(Button);
