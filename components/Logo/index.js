28e1d2b4bf3eba456751523ab6d5f19d8faa3994
Artur Mucha <arti.mucha@gmail.com>
2022-07-07 10:17:42 +0200

    add Logo component

---
+++ components/Logo/index.js
@@ -0,0 +1,11 @@
+import * as S from './styles';
+
+const Logo = ({
+  color = 'light',
+}) => (
+  <S.Wrapper color={color}>
+    angry nerds
+  </S.Wrapper>
+)
+
+export default Logo;
---
+++ components/Logo/styles.js
@@ -0,0 +1,17 @@
+import styled, { css } from 'styled-components';
+
+export const Wrapper = styled.h1`
+  ${({ theme, color }) => css`
+    width: calc(${theme.spacings.xxlarge} * 2);
+    width: max-content;
+    height: ${theme.spacings.xlarge};
+    line-height: calc(${theme.spacings.large} * 2);
+    color: ${theme.colors[color]};
+    font-size: ${theme.font.sizes.medium};
+    text-align: center;
+    background-image: url('/logo_${color}.png');
+    background-size: contain;
+    background-position: 50% 0;
+    background-repeat: no-repeat;
+  `}
+`
