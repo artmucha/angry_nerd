e32d1428c3b14e1ead76a876d29a09d57f21531f
Artur Mucha <arti.mucha@gmail.com>
2022-07-11 09:16:32 +0200

    add dropdown component

---
+++ components/Dropdown/index.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+import * as S from './styles';
+
+const Dropdown = ({ title = 'Ups', children }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  useEffect(() => {
+    document.body.style.overflow = isOpen ? 'hidden' : 'unset'
+
+    return () => {
+      document.body.style.overflow = 'unset'
+    }
+  }, [isOpen])
+
+  return (
+    <S.Wrapper isOpen={isOpen}>
+      <S.Title onClick={() => setIsOpen(!isOpen)}>{title}</S.Title>
+
+      <S.Content aria-hidden={!isOpen}>{children}</S.Content>
+      <S.Overlay aria-hidden={!isOpen} onClick={() => setIsOpen(!isOpen)} />
+    </S.Wrapper>
+  )
+};
+
+export default Dropdown;
