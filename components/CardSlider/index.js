48759a275bc3757c4ad0f15c5b6509b28facca4e
Artur Mucha <arti.mucha@gmail.com>
2022-07-30 08:51:49 +0200

    add CardSlider comp

---
+++ components/CardSlider/index.js
@@ -0,0 +1,58 @@
+import { ArrowBackIos as ArrowLeft } from '@styled-icons/material-outlined/ArrowBackIos';
+import { ArrowForwardIos as ArrowRight } from '@styled-icons/material-outlined/ArrowForwardIos';
+
+import Slider from 'components/Slider';
+import Card from 'components/Card';
+
+import * as S from './styles'
+
+const settings = {
+  arrows: true,
+  slidesToShow: 4,
+  infinite: false,
+  lazyLoad: 'ondemand',
+  responsive: [
+    {
+      breakpoint: 1375,
+      settings: {
+        arrows: false,
+        slidesToShow: 3.2
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        arrows: false,
+        slidesToShow: 2.2
+      }
+    },
+    {
+      breakpoint: 570,
+      settings: {
+        arrows: false,
+        slidesToShow: 1.2
+      }
+    },
+    {
+      breakpoint: 375,
+      settings: {
+        arrows: false,
+        slidesToShow: 1.1
+      }
+    }
+  ],
+  nextArrow: <ArrowRight aria-label="next games" />,
+  prevArrow: <ArrowLeft aria-label="previous games" />
+}
+
+const CardSlider = ({ items, color = 'white' }) => (
+  <S.Wrapper color={color}>
+    <Slider settings={settings}>
+      {items.map((item, index) => (
+        <Card key={index} {...item} />
+      ))}
+    </Slider>
+  </S.Wrapper>
+)
+
+export default CardSlider;
